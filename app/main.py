230be9c8a0e6c4512acba471f46830dd069f45ff
Trisna Utama <trisnautama@gmail.com>
2025-08-07 22:37:08 +0700

    init

--- app/main.py
+++ app/main.py
@@ -8,25 +8,16 @@ from app.core.settings import settings
 from app.routes.api import api_router
 
 app = FastAPI(
-    title=settings.APP_NAME,
-    docs_url=settings.DOCS_URL,
-    redoc_url=settings.REDOC_URL,
-    openapi_url=settings.OPENAPII_URL
+    title=settings.APP_NAME, docs_url=settings.DOCS_URL, redoc_url=settings.REDOC_URL, openapi_url=settings.OPENAPII_URL
 )
 
 if not Path(settings.UPLOADS_DIR).is_absolute():
-    app.mount(
-        settings.UPLOADS_URL_PATH,
-        StaticFiles(directory=settings.UPLOADS_DIR),
-        name="uploads"
-    )
+    app.mount(settings.UPLOADS_URL_PATH, StaticFiles(directory=settings.UPLOADS_DIR), name="uploads")
 
 
 @app.get("/scalar")
 def get_scalar():
-    return get_scalar_api_reference(
-        title=settings.APP_NAME,
-        openapi_url=settings.OPENAPII_URL
-    )
+    return get_scalar_api_reference(title=settings.APP_NAME, openapi_url=settings.OPENAPII_URL)
+
 
 app.include_router(api_router, prefix="/api/v1")
